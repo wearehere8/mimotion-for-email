cc203533a246553c1165873a6707c54fb80fed78
Will <32786169+wearehere8@users.noreply.github.com>
2022-11-19 18:15:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def push_wx(desp=""):
         print(sckey == "NO")
         return
     else:
-        server_url = f"https://sc.ftqq.com/{sckey}.send"
+        server_url = f"https://sctapi.ftqq.com/{sckey}.send"
         params = {
             "text": '【✍小米运动步数修改✍】',
             "desp": desp
