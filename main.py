99bcf35e5af69b467fa68b9f1263c8a8566e8477
Will <32786169+wearehere8@users.noreply.github.com>
2022-12-07 13:58:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def push_wx(step, desp=""):
     if sckey == 'NO':
         print(sckey == "NO")
         return
-    if int(step) <= 5000:
+    elseif step <= 5000:
         print("步数<=5000，不推送通知")
         return
     else:
