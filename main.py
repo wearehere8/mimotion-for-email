f80a083b69189af7b4cb4bbff6d36a9168468b3c
Will <32786169+wearehere8@users.noreply.github.com>
2023-02-27 21:54:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -224,10 +224,14 @@ def main(_user, _passwd, min_1, max_1):
 #     t = response['currentTime2']
 #     return t
 def get_time():
+    try:
         url = "http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp"
         response = requests.get(url, headers=self.headers).json()
         t = response["data"]["t"]
         return t
+    except Exception as e:
+        print(e)
+        return
 
 
 # 获取app_token
